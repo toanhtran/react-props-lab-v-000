dbc846579e6a5105002bdd0c326a0e0dc6d0457b
ToAnh Tran <toanh.t.tran@gmail.com>
2019-02-24 23:41:01 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,1 +1,7 @@
 // Code The Spaceship Component Here
+class Spaceship extends React.Component {
+  const stuff = {"name", speed, hasRockets=false, colors=["black","red"]}
+}
+
+
+export default Spaceship;
